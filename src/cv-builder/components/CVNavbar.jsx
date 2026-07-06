f4d7086cdae774864be69e076cde18dda112ae28
Daniel Nwankwo <danielnwankwo7@gmail.com>
2026-07-06 14:55:57 +0100

    redesign: clean professional CV navbar with grouped template dropdown, brand colors

--- src/cv-builder/components/CVNavbar.jsx
+++ src/cv-builder/components/CVNavbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TEMPLATES, ACCENT, FONT_OPTIONS, THEME_PRESETS } from "../utils/constants";
 import { TEMPLATE_INFO } from "../utils/sampleCV";
